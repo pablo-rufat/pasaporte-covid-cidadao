1cf52a05c81e02ee4421c26df09d0520506a37c4
gayapedro <pedrogaya@gmail.com>
2021-06-02 05:19:03 -0300

    fetch from db, create and send to blockchain

--- src/App.js
+++ src/App.js
@@ -1,81 +1,86 @@
-import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Web3 from "web3";
-import { Auth, API, graphqlOperation } from 'aws-amplify';
-import { abi, contractAddress } from "./utils/abi";
+import { name, br } from "faker-br";
+import { Auth, API, graphqlOperation } from "aws-amplify";
 import { getCidadao } from "./graphql/queries";
-import { Button, Card, CardActions, CardContent, CardHeader, CircularProgress, List, ListItem, ListItemText, Tooltip } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemText,
+  Tooltip,
+} from "@material-ui/core";
+import abi from "./utils/abi.json";
+import contractAddress from "./utils/contractAddress.json";
 import QRCode from "qrcode.react";
 
-const web3 = new Web3(
-  new Web3.providers.HttpProvider("http://localhost:8545")
-);
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const contrato = new web3.eth.Contract(abi, contractAddress);
 
 function App() {
-
-  const [ userData, setUserData ] = useState(null);
-  const [ dose1, setDose1 ] = useState(null);
-  const [ dose2, setDose2 ] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [dose1, setDose1] = useState(null);
+  const [dose2, setDose2] = useState(null);
   const [modalAberto, setModalAberto] = useState(false);
-  const [ userId, setUserId ] = useState(null);
-  const [ loading, setLoading ] = useState(true);
+  const [userId, setUserId] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [addUser, setAddUser] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
+      setLoading(true);
+      const currentUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
 
-      const currentUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
-
-      if (currentUser) {
-
-        setUserId(currentUser.attributes.sub);
-
+      if (currentUser && !userData) {
         const apiResponse = await API.graphql(
-          graphqlOperation(
-            getCidadao,
-            {
-              id: currentUser.attributes.sub
-            }
-          )
-        );
+          graphqlOperation(getCidadao, {
+            id: currentUser.attributes.sub,
+          })
+        ).catch(console.log());
+        const info = apiResponse.data.getCidadao;
 
-        setUserData(apiResponse.data.getCidadao);
-
-        if ( userData ) {
-
-          const contrato = new web3.eth.Contract(abi, contractAddress);
-
-          // get historicos
-
-
-          // await contrato.methods
-          //   .timestampPrimeiraDose()
-          //   .call({ from: userData.address })
-          //   .catch(e => console.log(e))
-          //   .then(result => {
-          //     setDose1(Number(result));
-          //   });
-
-          // await contrato.methods
-          //   .timestampSegundaDose()
-          //   .call({ from: userData.address })
-          //   .catch(e => console.log(e))
-          //   .then(result => {
-          //     setDose2(Number(result));
-          //   });
+        if (!info) setAddUser(true);
+        else {
+          console.log("entrou");
+          console.log(info);
+          setAddUser(false);
+          setUserData(info);
+          await web3.eth.personal
+            .unlockAccount(info.address, info.name, 10000)
+            .catch(console.log);
         }
-
-        console.log(userData);
       }
-
       setLoading(false);
     };
-
-    fetchUser();
+    if (!userData) {
+      fetchUser();
+    }
   }, []);
+
+  useEffect(() => {
+    const saveDB = async () => {
+      if(addUser){
+        const currentUser = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+
+      }
+    };
+
+
+  },[]);
 
   const logout = async () => {
     try {
@@ -86,21 +91,18 @@ function App() {
       await Auth.signOut();
       window.location.reload();
     } catch (error) {
-        console.log('error signing out: ', error);
+      console.log("error signing out: ", error);
     }
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       <div className={modalAberto ? "overlay" : "esconder"}>
         <div className='stats'>
           <button onClick={() => setModalAberto(false)} className='fecharModal'>
             &times;
           </button>
-          <Form
-            userId={userId}
-            setModalAberto={setModalAberto}
-          />
+          <Form userId={userId} setModalAberto={setModalAberto} />
         </div>
       </div>
       <Header
@@ -108,8 +110,9 @@ function App() {
         registered={!!userData}
         logout={logout}
       />
-      { loading && <CircularProgress /> }
-      { !loading && !!userData && <main>
+      {loading && <CircularProgress />}
+      {!loading && !!userData && (
+        <main>
           <Card>
             <CardHeader
               title={userData ? userData.name : ""}
@@ -119,23 +122,35 @@ function App() {
               <QRCode value={userData.address} />
               <List>
                 <ListItem>
-                  <ListItemText primary="Primeira dose" secondary={userData && dose1 ? dose1 : "Ainda nada"} />
+                  <ListItemText
+                    primary='Primeira dose'
+                    secondary={userData && dose1 ? dose1 : "Ainda nada"}
+                  />
                 </ListItem>
                 <ListItem>
-                  <ListItemText primary="Primeira dose" secondary={userData && dose2 ? dose2 : "Ainda nada"} />
+                  <ListItemText
+                    primary='Primeira dose'
+                    secondary={userData && dose2 ? dose2 : "Ainda nada"}
+                  />
                 </ListItem>
               </List>
             </CardContent>
             <CardActions>
-              <Tooltip title="Copiar endereço">
-                <Button size="small" color="primary" onClick={navigator.clipboard.writeText(userData ? userData.address : "")}>
+              <Tooltip title='Copiar endereço'>
+                <Button
+                  size='small'
+                  color='primary'
+                  onClick={navigator.clipboard.writeText(
+                    userData ? userData.address : ""
+                  )}
+                >
                   {userData ? userData.address : ""}
                 </Button>
               </Tooltip>
             </CardActions>
           </Card>
         </main>
-      }
+      )}
       <Footer />
     </div>
   );
